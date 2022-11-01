b5941c9da7bd3c5e839a0bbe04dba328df800310
Dariya Lavrenteva <lya-lya91@yandex.ru>
2022-11-01 23:12:08 +0300

    Test

--- src/main/java/ru/yandex/praktikum/history/InMemoryHistoryManager.java
+++ src/main/java/ru/yandex/praktikum/history/InMemoryHistoryManager.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private final int COUNT_TASK_IN_LIST_HISTORY = 10;
+    private final int COUNT_TASK_IN_LIST_HISTORY = 11;
     private final List<Task> taskListHistory = new LinkedList<>();
     private final int INDEX_FOR_REMOVE = 0;
 
